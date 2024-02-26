178df4405a01734f31ffb8c91ea8279628b6a2b3
shreyas-raj01 <114733870+shreyas-raj01@users.noreply.github.com>
2024-02-26 16:51:18 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import "../styles/components/about.css";
+import "../styles/components/contact.css";
+import "../styles/components/featured.css";
+import "../styles/components/footer.css";
+import "../styles/components/header.css";
+import "../styles/components/hero.css";
+import "../styles/components/mobile-nav.css";
+import "../styles/components/work.css";
+import "../styles/modern-normalize.css";
+import "../styles/style.css";
+import "../styles/utils.css";
+import darkMode from "./utils/dark-mode";
+import mobileNav from "./utils/mobile-nav";
+
+mobileNav();
+darkMode();
